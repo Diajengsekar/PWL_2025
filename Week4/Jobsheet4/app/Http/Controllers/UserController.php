704b7bb7269d40a9f4fa05d60b9eb5af6d406945
Diajengsekar <diajengsekararum31@gmail.com>
2025-03-08 21:14:11 +0700

    Praktikum 2.1

--- Week4/Jobsheet4/app/Http/Controllers/UserController.php
+++ Week4/Jobsheet4/app/Http/Controllers/UserController.php
@@ -9,17 +9,11 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-{
-    $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('12345')
-    ];
+    {
+        $user = UserModel::findOrFail(20, ['username', 'nama'], function () {
+            abort(404);
+        });
 
-    UserModel::create($data);
-
-    $user = UserModel::all();
-    return view('user', ['data' => $user]);
-}
+        return view('user', ['data' => $user]);
+    }
 }
