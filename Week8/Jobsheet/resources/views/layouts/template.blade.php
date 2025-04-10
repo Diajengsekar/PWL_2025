08052c404710bc0e712553fa7372795cb75b75a7
Diajengsekar <diajengsekararum31@gmail.com>
2025-04-10 18:57:01 +0700

    Praktikum 1 - Implementasi Upload File untuk import data

--- Week8/Jobsheet/resources/views/layouts/template.blade.php
+++ Week8/Jobsheet/resources/views/layouts/template.blade.php
@@ -79,7 +79,7 @@
 
 <!-- jquery-validation -->
 <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.js')}}"></script>
-<script src="{{ asset('plugins/jquery-validation/additional-methods.min.js')}}"></script>
+<script src="{{ asset('adminlte/plugins/jquery-validation/additional-methods.min.js')}}"></script>
 
 <!-- SweetAlert2 -->
 <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
