e0f02d84a81882aa6f05a2be4b0bb94ab4811285
Diajengsekar <diajengsekararum31@gmail.com>
2025-04-11 11:19:03 +0700

    Tugas 3 - Export Pdf Kategori

--- Week8/Jobsheet/resources/views/kategori/index.blade.php
+++ Week8/Jobsheet/resources/views/kategori/index.blade.php
@@ -8,6 +8,7 @@
             <button onclick="modalAction(`{{ url('kategori/import') }}`)" class="btn btn-info">Import Kategori</button>
             <a href="{{ url('/kategori/export_excel') }}" class="btn btn-primary">Export Kategori</a>
             <button onclick="modalAction(`{{ url('kategori/create_ajax') }}`)" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+            <a href="{{ url('/kategori/export_pdf') }}" class="btn btn-warning"><i class="fa fa-filepdf"></i> Export Kategori</a> 
         </div>
         </div>
         
