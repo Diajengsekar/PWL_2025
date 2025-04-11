ea502727a0a95a79da4f358666d02f421d7c1c97
Diajengsekar <diajengsekararum31@gmail.com>
2025-04-11 11:23:38 +0700

    Tugas 3 - Export Pdf Supplier

--- Week8/Jobsheet/resources/views/supplier/index.blade.php
+++ Week8/Jobsheet/resources/views/supplier/index.blade.php
@@ -8,6 +8,7 @@
             <button onclick="modalAction(`{{ url('supplier/import') }}`)" class="btn btn-info">Import Supplier</button>
             <a href="{{ url('/supplier/export_excel') }}" class="btn btn-primary">Export Supplier</a>
             <button onclick="modalAction(`{{ url('supplier/create_ajax') }}`)" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+            <a href="{{ url('/supplier/export_pdf') }}" class="btn btn-warning"><i class="fa fa-filepdf"></i> Export Supplier</a>
         </div>
         </div>
 
