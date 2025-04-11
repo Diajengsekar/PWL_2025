9d57040db0593bd5cc762c93b511cd7bee3347c2
Diajengsekar <diajengsekararum31@gmail.com>
2025-04-11 11:44:42 +0700

    Tugas 4 - Implementasi Upload File Gambar

--- Week8/Jobsheet/routes/web.php
+++ Week8/Jobsheet/routes/web.php
@@ -19,11 +19,15 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->n
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'postRegister']);
 
-
+    
+    
 // Semua rute di bawah ini hanya bisa diakses jika sudah login
 Route::middleware(['auth'])->group(function () {
     Route::get('/', [WelcomeController::class, 'index']);
 
+    Route::get('/profile', [UserController::class, 'profile']);
+    Route::post('/profile/update-photo', [UserController::class, 'updatePhoto'])->name('user.updatePhoto');
+    
     Route::middleware(['authorize:ADM'])->group(function(){
         Route::group(['prefix' => 'user'], function () {
             Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
