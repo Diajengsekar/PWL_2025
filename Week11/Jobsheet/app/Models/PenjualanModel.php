c8da60a9dd741ffb87f24c4d9ce8d5e2d33007f7
Diajengsekar <diajengsekararum31@gmail.com>
2025-05-06 21:11:07 +0700

    Tugas - Implementasi Eloquent Accessor Image (Transaksi)

--- Week11/Jobsheet/app/Models/PenjualanModel.php
+++ Week11/Jobsheet/app/Models/PenjualanModel.php
@@ -8,9 +8,21 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\UserModel;
 use App\Models\DetailPenjualanModel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable; 
 
 class PenjualanModel extends Model
 {
+
+    public function getJWTIdentifier(){ 
+        return $this->getKey();
+    }
+    
+    public function getJWTCustomClaims(){ 
+        return [];
+    }
+
     use HasFactory;
     protected $table = 't_penjualan';
     protected $primaryKey = 'penjualan_id';
@@ -19,7 +31,7 @@ class PenjualanModel extends Model
      *
      * @var array
      */
-    protected $fillable = ['user_id', 'pembeli', 'penjualan_kode', 'penjualan_tanggal',];
+    protected $fillable = ['user_id', 'pembeli', 'penjualan_kode', 'penjualan_tanggal', 'image'];
 
     //Relasi ke tabel user
     public function user(): BelongsTo
@@ -33,8 +45,15 @@ class PenjualanModel extends Model
 
 
      // Menambahkan method untuk mendapatkan nama
-     public function getUsernameAttribute()
-     {
+        public function getUsernameAttribute()
+        {
          return $this->user->nama ?? null;  // Ambil nama dari relasi user
-     }
+        }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => $image ? url('/storage/transaksi/' . $image) : null
+        );
+    }
 }
