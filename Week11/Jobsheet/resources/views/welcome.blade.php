8b53f22548a8cc7ef78a9f3e92ed717fe9fc5c74
Diajengsekar <diajengsekararum31@gmail.com>
2025-05-06 20:25:03 +0700

    add js 11

---
+++ Week11/Jobsheet/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h3 class="card-title">Halo!!</h3>
+        <div class="card-tools"></div>
+    </div>
+    <div class="card-body">
+        Selamat datang, ini halaman utama Website POS
+    </div>
+</div>
+
+@endsection
